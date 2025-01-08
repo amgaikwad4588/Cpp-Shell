339651a88df4800484cf6c23bfb0e6b0a1d46e03
amgaikwad4588 <91586360+amgaikwad4588@users.noreply.github.com>
2025-01-09 01:14:01 +0530

    Add files via upload
    Echo added

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <string>
 using namespace std;
+
 int main()
 {
-  // Flush after every std::cout / std:cerr
+  // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
@@ -11,8 +13,22 @@ int main()
     std::cout << "$ ";
     std::string input;
     std::getline(std::cin, input);
+
     if (input == "exit 0")
-      exit(0);
-    std::cout << input << ": command not found" << std::endl;
+    {
+      exit(0); // Exit the program if "exit 0" is entered
+    }
+    else if (input.find("echo ") == 0)
+    {
+      const int ECHO_LEN = 5; // Including space
+      std::string text = input.substr(ECHO_LEN);
+      std::cout << text << std::endl;
+    }
+    else
+    {
+      std::cout << input << ": command not found" << std::endl;
+    }
   }
+
+  return 0;
 }
