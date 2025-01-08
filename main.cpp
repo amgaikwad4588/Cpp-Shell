a0bfc081e23a2abe3730f75a5bd86893dd54923b
amgaikwad4588 <91586360+amgaikwad4588@users.noreply.github.com>
2025-01-09 00:54:40 +0530

    Add files via upload
    Exit(0) command added

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  // Flush after every std::cout / std:cerr
+  std::cout << std::unitbuf;
+  std::cerr << std::unitbuf;
+
+  while (true)
+  {
+    std::cout << "$ ";
+    std::string input;
+    std::getline(std::cin, input);
+    if (input == "exit 0")
+      exit(0);
+    std::cout << input << ": command not found" << std::endl;
+  }
+}
